641f283d82be0644b5165d733ff55cfb764bdda1
Filip Chalupa <chalupa.filip@gmail.com>
2022-11-29 00:41:28 +0100

    Hamburger menu

--- film.js
+++ film.js
@@ -104,3 +104,15 @@ const filmy = [
 		premiera: '2022-12-24',
 	},
 ]
+
+const menuTlacitkoElement = document.querySelector('#menu-tlacitko')
+const menuPolozkyElement = document.querySelector('#menu-polozky')
+menuTlacitkoElement.addEventListener('click', () => {
+	menuPolozkyElement.classList.toggle('show')
+
+	if (menuPolozkyElement.classList.contains('show')) {
+		menuTlacitkoElement.innerHTML = '<i class="fas fa-xmark"></i>'
+	} else {
+		menuTlacitkoElement.innerHTML = '<i class="fas fa-bars"></i>'
+	}
+})
